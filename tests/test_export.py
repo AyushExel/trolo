6aec1e2a81e3dcb3fd324a8ba1b6fea0ccd1959e
harikdava <hardik1901dava@gmail.com>
2024-12-13 16:18:45 +0100

    test modified and asserts added

--- tests/test_export.py
+++ tests/test_export.py
@@ -1,7 +1,6 @@
 import pytest
-import torch
-import onnx
-import numpy as np
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
 from trolo.export import ModelExporter
 from trolo.utils.smart_defaults import infer_pretrained_model
@@ -16,48 +15,46 @@ def model_path():
 def exporter(model_path):
     return ModelExporter(DEFAULT_MODEL)
 
+@pytest.fixture
+def create_temp_dir():
+    with TemporaryDirectory() as temp_dir:
+        temp_dir = Path(temp_dir)
+        yield temp_dir
+
 def test_onnx_export(exporter, tmp_path):
     input_size = (640, 640)
-    default_dynamic_axes = {'images': {0: 'N'}, 'orig_target_sizes': {0: 'N'}}
-    
-    # formatted_data = format_input(input_data)
-    
     exporter.export(
         export_format='onnx', 
         input_size=input_size
     )
+    assert Path(DEFAULT_MODEL).with_suffix('.onnx').exists()
 
 @pytest.mark.parametrize("input_size", [
     (640, 640)
 ])
 def test_export_variable_input_sizes(exporter, input_size):
-    default_dynamic_axes = {'images': {0: 'N'}, 'orig_target_sizes': {0: 'N'}}
-    
     exporter.export(
         export_format='onnx', 
         input_size=input_size       
     )
+    assert Path(DEFAULT_MODEL).with_suffix('.onnx').exists()
 
 def test_export_with_custom_dynamic_axes(exporter):
     input_size = (640, 640)
-    custom_dynamic_axes = {
-        'images': {0: 'batch', 2: 'height', 3: 'width'},
-        'orig_target_sizes': {0: 'batch'}
-    }
-    
     exporter.export(
         export_format='onnx', 
         input_size=input_size
     )
+    assert Path(DEFAULT_MODEL).with_suffix('.onnx').exists()
 
 def test_export_with_simplification(exporter):
     input_size = (640, 640)
-    default_dynamic_axes = {'images': {0: 'N'}, 'orig_target_sizes': {0: 'N'}}
-    
+
     exporter.export(
         export_format='onnx', 
         input_size=input_size
     )
+    assert Path(DEFAULT_MODEL).with_suffix('.onnx').exists()
 
 def test_export_invalid_format(exporter):
     input_size = (640, 640)
@@ -68,3 +65,21 @@ def test_export_invalid_format(exporter):
             export_format=None, 
             input_size=input_size            
         )
+
+def test_openvino_export(exporter, tmp_path):
+    input_size = (640, 640)
+
+    exporter.export(
+        export_format='openvino',
+        input_size=input_size
+    )
+    assert Path(DEFAULT_MODEL).with_suffix('.xml').exists()
+
+def test_openvino_export_fp16(exporter, tmp_path):
+    input_size = (640, 640)
+    exporter.export(
+        export_format='openvino',
+        input_size=input_size,
+        fp16=True,
+    )
+    assert Path(DEFAULT_MODEL).with_suffix('.xml').exists()
